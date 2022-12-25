e5fe303b0a48d8c3a435d4c6835311b0f63eb526
Andy Bohn <andy@andybohn.com>
2022-12-25 10:24:48 -0800

    Day2 star1

---
+++ 2022/rust/day2/src/main.rs
@@ -0,0 +1,62 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn get_file(filename: &str) -> File {
+    let file: File = match File::open(&filename) {
+        Ok(file) => file,
+        Err(err) => {
+            panic!("Error opening {}: {}", filename, err);
+        }
+    };
+
+    return file;
+}
+
+fn get_play_as_int(play: char, base: char) -> i32 {
+    return ((play as u32) - (base as u32) + 1) as i32;
+}
+
+fn score_round(opp_play: char, strat_play: char) -> i32 {
+    let opp_num = get_play_as_int(opp_play, 'A');
+    let my_num = get_play_as_int(strat_play, 'X');
+
+    let outcome = (opp_num - my_num).rem_euclid(3);
+    let outcome_score = match outcome {
+        0 => 3, // draw
+        1 => 0, // lose
+        2 => 6, // win, e.g. I play rock, they play paper
+        _ => panic!(),
+    };
+
+    return my_num + outcome_score;
+}
+
+fn main() {
+    assert!(process("test") == 15, "Failed testcase");
+    process("input");
+}
+
+fn process(input: &str) -> i32 {
+    let reader = BufReader::new(get_file(input));
+
+    // Round 1:
+    // An elf gave us a strategy guide with predictions and recommendations
+    // for a rock paper scissors tourney. Highest score wins.
+    //
+    // Score is shape_score + outcome_score
+    // shape_score 1 rock, 2 paper, 3 scissors
+    // outcome_score 0 lose, 3 draw, 6 win
+    let mut score = 0;
+    for (_, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+
+        let mut round_iter = line.split_whitespace();
+        let opp_play = round_iter.next().unwrap().chars().next().unwrap();
+        let strat_play = round_iter.next().unwrap().chars().next().unwrap();
+
+        score += score_round(opp_play, strat_play);
+    }
+
+    println!("{} {}", input, score);
+    return score;
+}
