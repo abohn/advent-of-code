137b3e5e031bd6aee0c4ba726a6d746a2c4fd36b
Andy Bohn <andy@andybohn.com>
2023-01-02 17:24:34 -0800

    day10 down

--- 2022/rust/day10/src/main.rs
+++ 2022/rust/day10/src/main.rs
@@ -36,31 +36,50 @@ fn parse_input(input: &str) -> Vec<Instruction> {
         .collect::<Vec<Instruction>>()
 }
 
-fn process(instructions: &Vec<Instruction>) -> i32 {
-    let mut cycle: i32 = 0;
-    let mut x = 1;
+fn process(instructions: &Vec<Instruction>) -> (i32, String) {
+    let mut sprite_pos = 1;
     let mut acc = 0;
+    let mut cycle: i32 = 0;
+    let mut display: Vec<char> = Vec::new();
+
+    let screen_width = 40;
 
     for inst in instructions.iter() {
         for _ in 0..inst.cycles() {
             cycle += 1;
-            if (cycle - 20).rem_euclid(40) == 0 {
-                acc += cycle * x;
+            let scan_pos = (cycle - 1).rem_euclid(screen_width);
+
+            // p1
+            if (cycle - screen_width / 2).rem_euclid(screen_width) == 0 {
+                acc += cycle * sprite_pos;
             }
+
+            // p2
+            if scan_pos == 0 && cycle != 1 {
+                display.push('\n');
+            }
+            display.push(if (scan_pos - sprite_pos).abs() <= 1 {
+                '#'
+            } else {
+                '.'
+            });
         }
 
         match inst {
-            Instruction::Addx(val) => x += val,
+            Instruction::Addx(val) => sprite_pos += val,
             Instruction::Noop => (),
         }
     }
+    display.push('\n');
 
-    acc
+    (acc, display.into_iter().collect())
 }
 
 fn main() {
     let input = parse_input(include_str!["../input"]);
-    println!("{}", process(&input));
+
+    let (signal_strength_acc, display) = process(&input);
+    println!("{}\n{}", signal_strength_acc, display);
 }
 
 #[cfg(test)]
@@ -68,6 +87,19 @@ mod tests {
     #[test]
     fn test() {
         let input = super::parse_input(include_str!["../test"]);
-        assert_eq!(super::process(&input), 13140);
+        assert_eq!(
+            super::process(&input),
+            (
+                13140,
+                "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+"
+                .to_string()
+            )
+        );
     }
 }
